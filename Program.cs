9dcfb0725b52b45441c21f209cd3d87788c8e875
cecifernandez <mceciliafernandezst@gmail.com>
2025-03-19 10:23:30 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,65 @@
-﻿namespace POO_practica
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace POO_practica
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var botellaDeAgua = new Botella();
+            botellaDeAgua.contenido = "Agua";
+            botellaDeAgua.color = "Rojo";
+            botellaDeAgua.tamañoCm = 20;
+            botellaDeAgua.litros = 1;
+
+            botellaDeAgua.Tomar();
+            botellaDeAgua.Llenar();
+
+            var lapiceraNegra = new Lapicera();
+            lapiceraNegra.color = "Gris";
+            lapiceraNegra.tamañoCm = 10;
+            lapiceraNegra.colorTinta = "Negra";
+            lapiceraNegra.tipoTrazo = "Fino";
+
+            lapiceraNegra.Escribir();
+            lapiceraNegra.Dibujar();
+
+        }
+
+        public class Botella()
+        {
+            public string contenido;
+            public string color;
+            public int tamañoCm;
+            public int litros;
+
+            public void Tomar()
+            {
+                Console.WriteLine("Tomando " + contenido + " de la botella");
+            }
+
+            public void Llenar()
+            {
+                Console.WriteLine("Llenando botella");
+            }
+        }
+
+        public class Lapicera()
+        {
+            public string color;
+            public int tamañoCm;
+            public string colorTinta;
+            public string tipoTrazo;
+
+            public void Escribir()
+            {
+                Console.WriteLine("Escribiendo");
+            }
+            public void Dibujar()
+            {
+                Console.WriteLine("Dibujando");
+            }
+
         }
     }
 }
